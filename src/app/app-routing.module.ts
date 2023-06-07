904e14ff9b8848cd39929509260b4eefa620710a
Aritra Basu <aritra.basu123@gmail.com>
2023-06-07 18:35:03 +0530

    change in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminProfileComponent } from './admin-profile/admin-profile.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
 import { RegisterComponent } from './register/register.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent,
     children:[
+      {path: 'patient-dashboard', component: PatientDashboardComponent}
     ]
   }
 ];
